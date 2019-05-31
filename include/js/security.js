61eba797702fee380e8f89b525bb8dbc3428e8d5
Tony Yang <t510599@gmail.com>
2019-06-01 01:12:36 +0800

    fixed csrf token didn't work in admin panel

--- include/js/security.js
+++ include/js/security.js
@@ -3,7 +3,7 @@ axios.defaults.withCredentials = true;
 axios.interceptors.request.use(function (config) {
     var crypto = window.crypto || window.msCrypto;
     let csrfToken = btoa(String(crypto.getRandomValues(new Uint32Array(1))[0]));
-    document.cookie = `${axios.defaults.xsrfCookieName}=${csrfToken}`;
+    document.cookie = `${axios.defaults.xsrfCookieName}=${csrfToken}; max-age=10; path=/`;
     return config;
 }, function (error) {
     return Promise.reject(error);
