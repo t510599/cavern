45b778484a5cbaa7c420e8f998274f4613f2146c
Tony Yang <t510599@gmail.com>
2019-07-03 09:11:28 +0800

    fixed notifying non-existent user

--- include/notification.php
+++ include/notification.php
@@ -1,6 +1,12 @@
 <?php
 function cavern_notify_user($username, $message="你有新的通知!", $url="", $type="") {
     global $SQL;
+    try {
+        // do not notify the user that doesn't exist
+        $user = new User($username);
+    } catch (NoUserException $e) {
+        return $e;
+    }
     $time = date('Y-m-d H:i:s');
     $SQL->query("INSERT INTO `notification` (`username`, `message`, `url`, `type`, `time`) VALUES ('%s', '%s', '%s', '%s', '%s')", array($username, $message, $url, $type, $time));
 }
