b653b604d7d694c4a7d1d02de5782868e9c6756b
Tony Yang <t510599@gmail.com>
2020-06-06 23:38:15 +0800

    Fixed katex library repeatly-loaed in index.php

--- include/js/cards.js
+++ include/js/cards.js
@@ -4,19 +4,27 @@ const cdnjs = "https://cdnjs.cloudflare.com/ajax/libs";
 const libraries = [
     cdnjs + "/marked/0.5.1/marked.min.js",
     cdnjs + "/prettify/r298/prettify.min.js",
-    cdnjs + "/underscore.js/1.9.1/underscore-min.js"
+    cdnjs + "/underscore.js/1.9.1/underscore-min.js",
+    editormd.katexURL.js + ".js",
 ];
 
 loadJS(libraries).then(function () {
+    // attach libraries to editormd
     editormd.$marked = marked;
-    editormd.loadFiles.js.push(...libraries.map(url => url.slice(0, -3))); // remove ".js"
+
+    editormd.loadCSS(editormd.katexURL.css);
+    editormd.$katex = katex;
+    editormd.kaTeXLoaded = true;
+    editormd.loadFiles.js.push(...libraries.filter(url => url.endsWith("js")).map(url => url.slice(0, -3))); // remove ".js"
+    
     document.querySelectorAll('.ts.card .description').forEach(function(el) {
         let id = el.getAttribute('id');
         parseMarkdown(id, el.children[0].textContent, {
             toc: false,
             flowChart: false,
             sequenceDiagram: false,
-            htmlDecode : "script,iframe,style|on*"
+            htmlDecode : "script,iframe,style|on*",
+            autoLoadKatex: false
         }).children('.markdown').hide();
     });
     postProcess();
